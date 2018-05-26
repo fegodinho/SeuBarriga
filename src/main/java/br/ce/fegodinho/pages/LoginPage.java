1888ebe49e29c912dbb46c0ca867c230c3c0bae1
Felipe Godinho <fegodinho@gmail.com>
2018-05-26 14:29:22 -0300

    Aula 74 - Login

---
+++ src/main/java/br/ce/fegodinho/pages/LoginPage.java
@@ -0,0 +1,32 @@
+package br.ce.fegodinho.pages;
+
+import org.openqa.selenium.By;
+
+import br.ce.fegodinho.core.BasePage;
+import br.ce.fegodinho.core.DriverFactory;
+
+public class LoginPage extends BasePage {
+	
+	public void acessarTelaInicial() {
+		DriverFactory.getDriver().get("https://srbarriga.herokuapp.com");
+	}
+	
+	public void setEmail(String email) {
+		escreve("email", email);
+	}
+	
+	public void setSenha(String senha) {
+		escreve("senha", senha);
+	}
+	
+	public void entrar() {
+		clicarBotao(By.xpath("//button[.='Entrar']"));		
+	}
+	
+	public void logar(String email, String senha) {
+		setEmail(email);
+		setSenha(senha);
+		entrar();
+	}
+
+}
---
+++ src/main/java/br/ce/fegodinho/tests/ContaTest.java
@@ -0,0 +1,14 @@
+package br.ce.fegodinho.tests;
+
+import org.junit.Test;
+
+import br.ce.fegodinho.core.BaseTest;
+
+public class ContaTest extends BaseTest {
+	
+	@Test
+	public void test() {
+		
+	}
+
+}
