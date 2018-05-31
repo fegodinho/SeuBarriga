720b172a28772dc75a03db251dc1d216200a1dfc
Felipe Godinho <fegodinho@gmail.com>
2018-05-31 15:08:12 -0300

    Aula 84 - Resumo Mensal

--- src/main/java/br/ce/fegodinho/tests/ResumoTest.java
+++ src/main/java/br/ce/fegodinho/tests/ResumoTest.java
@@ -1,9 +1,12 @@
 package br.ce.fegodinho.tests;
 
+import static br.ce.fegodinho.core.DriverFactory.getDriver;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 import br.ce.fegodinho.core.BaseTest;
+import br.ce.fegodinho.core.DriverFactory;
 import br.ce.fegodinho.pages.MenuPage;
 import br.ce.fegodinho.pages.ResumoPage;
 
@@ -17,9 +20,14 @@ public class ResumoTest extends BaseTest {
 		
 		resumoPage.excluirMovimentacao();
 		
-		Assert.assertEquals("Movimentação removida com sucesso!", resumoPage.obterMensagemSucesso());
+		Assert.assertEquals("Movimentação removida com sucesso!", resumoPage.obterMensagemSucesso());		
+	}
+	
+	@Test
+	public void testResumoMensal() {
+		menuPage.acessarTelaResumo();
 		
-		
+		Assert.assertEquals("Seu Barriga - Extrato", getDriver().getTitle());
 	}
 
 }
