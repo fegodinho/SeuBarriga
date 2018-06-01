f0dbbe7876c67bd763165cc2fa98cf2c2834c4fa
Felipe Godinho <fegodinho@gmail.com>
2018-06-01 18:50:04 -0300

    Aula 88 - Teste do resumo mensal vazio

--- src/main/java/br/ce/fegodinho/tests/ResumoTest.java
+++ src/main/java/br/ce/fegodinho/tests/ResumoTest.java
@@ -2,12 +2,17 @@ package br.ce.fegodinho.tests;
 
 import static br.ce.fegodinho.core.DriverFactory.getDriver;
 
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 import br.ce.fegodinho.core.BaseTest;
+import br.ce.fegodinho.core.DriverFactory;
 import br.ce.fegodinho.pages.MenuPage;
 import br.ce.fegodinho.pages.ResumoPage;
 
@@ -25,11 +30,25 @@ public class ResumoTest extends BaseTest {
 		Assert.assertEquals("Movimentação removida com sucesso!", resumoPage.obterMensagemSucesso());		
 	}
 	
+	//Possivel solução 1
+	//@Test(expected=NoSuchElementException.class)
 	@Test
 	public void test2_ResumoMensal() {
 		menuPage.acessarTelaResumo();
 		
 		Assert.assertEquals("Seu Barriga - Extrato", getDriver().getTitle());
+		
+		List<WebElement> elementosEncontrados =
+				DriverFactory.getDriver().findElements(By.xpath("//*[@id='tabelaExtrato']/tbody/tr"));
+		
+		Assert.assertEquals(0, elementosEncontrados.size());
+		
+		//Possivel solucao 2 para verificar que a tabela está vazia
+		/*try {
+			DriverFactory.getDriver().findElement(By.xpath("//*[@id='tabelaExtrato']/tbody/tr"));
+			Assert.fail();
+		} catch (NoSuchElementException e) {
+			
+		}*/
 	}
-
 }
