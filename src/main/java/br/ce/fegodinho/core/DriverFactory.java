ad88db1176e7882c5f9c1f7e7bb46383e951fa46
Felipe Godinho <fegodinho@gmail.com>
2018-06-02 15:19:14 -0300

    Aula 92 - Um driver por thread

--- src/main/java/br/ce/fegodinho/core/DriverFactory.java
+++ src/main/java/br/ce/fegodinho/core/DriverFactory.java
@@ -9,29 +9,44 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class DriverFactory {
 	
-	private static WebDriver driver;
+	//private static WebDriver driver;
+	
+	private static ThreadLocal<WebDriver> threadDriver = new ThreadLocal<WebDriver>() {
+		@Override
+		protected synchronized WebDriver initialValue() {
+			return initDriver();
+		}
+	};
 	
 	private DriverFactory() {
 		
 	}
 	
-	public static WebDriver getDriver() {	
-		if (driver == null) {
-			switch (Propriedades.browser) {
+	public static WebDriver getDriver() {
+		return threadDriver.get();
+	}
+	
+	public static WebDriver initDriver() {	
+		WebDriver driver = null;
+		
+		switch (Propriedades.browser) {
 			case FIREFOX: driver = new FirefoxDriver(); break;
 			case CHROME: driver = new ChromeDriver(); break;
 			case INTERNET_EXPLORER: driver = new InternetExplorerDriver(); break;
 			case MICROSOFT_EDGE: driver = new EdgeDriver(); break;			
-			}
-			driver.manage().window().setSize(new Dimension(1200, 765));
-		}		
+		}
+		driver.manage().window().setSize(new Dimension(1200, 765));			
 		return driver;		
 	}
 	
 	public static void killDriver() {
+		WebDriver driver = getDriver();
 		if (driver != null) {
 		driver.quit();
 		driver=null;
+		}
+		if(threadDriver != null) {
+			threadDriver.remove();
 		}
 	}	
 }
--- src/main/java/br/ce/fegodinho/suites/SuiteGeral.java
+++ src/main/java/br/ce/fegodinho/suites/SuiteGeral.java
@@ -1,13 +1,9 @@
 package br.ce.fegodinho.suites;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import br.ce.fegodinho.core.DriverFactory;
-import br.ce.fegodinho.pages.LoginPage;
 import br.ce.fegodinho.tests.ContaTest;
 import br.ce.fegodinho.tests.MovimentacaoTest;
 import br.ce.fegodinho.tests.RemoverMovimentacaoContaTest;
@@ -22,21 +18,5 @@ import br.ce.fegodinho.tests.SaldoTest;
 	SaldoTest.class,
 	ResumoTest.class	
 })
-public class SuiteGeral {
-	
-	private static LoginPage page = new LoginPage();
-	
-	@BeforeClass
-	public static void Inicializa() {
-		page.acessarTelaInicial();
-		page.setEmail("felipe@godinho");
-		page.setSenha("123456");
-		page.entrar();
-	}
-	
-	@AfterClass
-	public static void finaliza() {
-		DriverFactory.killDriver();
-	}
-
+public class SuiteGeral {	
 }
