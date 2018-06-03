bf816032a9f1ecf126a4586c8ca4835e70310d7b
Felipe Godinho <fegodinho@gmail.com>
2018-06-03 15:15:00 -0300

    Aula 98 - Refatorando para usar o GRID

--- src/main/java/br/ce/fegodinho/core/DriverFactory.java
+++ src/main/java/br/ce/fegodinho/core/DriverFactory.java
@@ -1,11 +1,18 @@
 package br.ce.fegodinho.core;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import br.ce.fegodinho.core.Propriedades.TipoExecucao;
 
 public class DriverFactory {
 	
@@ -29,14 +36,32 @@ public class DriverFactory {
 	public static WebDriver initDriver() {	
 		WebDriver driver = null;
 		
-		switch (Propriedades.browser) {
+		if(Propriedades.TIPO_EXECUCAO == TipoExecucao.LOCAL) {		
+		switch (Propriedades.BROWSER) {
 			case FIREFOX: driver = new FirefoxDriver(); break;
 			case CHROME: driver = new ChromeDriver(); break;
 			case INTERNET_EXPLORER: driver = new InternetExplorerDriver(); break;
 			case MICROSOFT_EDGE: driver = new EdgeDriver(); break;			
+			}
 		}
+		if(Propriedades.TIPO_EXECUCAO == TipoExecucao.GRID) {
+			DesiredCapabilities cap = null;
+			switch (Propriedades.BROWSER) {
+			case FIREFOX: cap = DesiredCapabilities.firefox(); break;
+			case CHROME: cap = DesiredCapabilities.chrome(); break;
+			case INTERNET_EXPLORER: cap = DesiredCapabilities.internetExplorer(); break;
+			case MICROSOFT_EDGE: cap = DesiredCapabilities.edge(); break;			
+			}
+			try {
+				driver = new RemoteWebDriver(new URL("http://192.168.25.5:4444/wd/hub"), cap);
+			} catch (MalformedURLException e) {
+				System.err.println("Falha na conexão com o GRID");
+				e.printStackTrace();
+			}
+		}
+		
 		driver.manage().window().setSize(new Dimension(1200, 765));			
-		return driver;		
+		return driver;
 	}
 	
 	public static void killDriver() {
